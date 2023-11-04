8bc57879affc93f1e2df85ae8ff1ac6458301a73
Karanjeet-kumar <karanrajput8400@gmail.com>
2023-11-05 03:56:51 +0530

    looking_good

--- script.js
+++ script.js
@@ -1,28 +1,70 @@
 console.log("Welcome to Tic Tac Toe")
 let turn = "X"
+let num="1"
 let isgameover = false;
+var a=0;
 
 const changeTurn = ()=>{
-    return turn === "X"? "0": "X"
+    return turn === "X"? "O": "X"
+}
+const changeNum = ()=>{
+    return num === "1"? "2": "1"
 }
 
+const checkWin = ()=>{
+    let boxtext = document.getElementsByClassName('boxtext');
+    let wins = [
+        [0, 1, 2, 5, 5, 0],
+        [3, 4, 5, 5, 15, 0],
+        [6, 7, 8, 5, 25, 0],
+        [0, 3, 6, -5, 15, 90],
+        [1, 4, 7, 5, 15, 90],
+        [2, 5, 8, 15, 15, 90],
+        [0, 4, 8, 5, 15, 45],
+        [2, 4, 6, 5, 15, 135],
+    ]
+    wins.forEach(e =>{
+        if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "") ){
+            document.querySelector('.info').innerText = boxtext[e[0]].innerText + " Won"
+            isgameover = true
+        }
+    })
+}
+let  moves=0
 let boxes = document.getElementsByClassName("box");
 Array.from(boxes).forEach(element =>{
-    let boxtext = element.querySelector('.boxText');
+    let boxtext = element.querySelector('.boxtext');
     element.addEventListener('click', ()=>{
         if(boxtext.innerText === ''){
             boxtext.innerText = turn;
             turn = changeTurn();
+            num = changeNum();
+            moves+=1;
+            
             checkWin();
-            if (!isgameover){
-                document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
-            } 
+            if(!isgameover){
+                if(moves==9)
+                document.getElementsByClassName("info")[0].innerText  = "Draw";
+                else
+                document.getElementsByClassName("info")[0].innerText  = "Player "+num + "'s Turn (" + turn +")";
+
+            }
+               
         }
+        // else{
+        //     document.getElementsByClassName("info")[0].innerText = "Draw!";
+        // }
     })
 })
 
-const checkWin= ()=>{
-
-}
-
+reset.addEventListener('click', ()=>{
+    let boxtexts = document.querySelectorAll('.boxtext');
+    Array.from(boxtexts).forEach(element => {
+        element.innerText = ""
+    });
+    turn = "X";
+    num="1"; 
+    isgameover = false
+    document.getElementsByClassName("info")[0].innerText  = "Player "+num + "'s Turn (" + turn +")";
+})
 
